caba32baf22a2caedab127df4978b8c386d9c8ad
Matteo Paoli <matteo_paoli@outlook.com>
2022-07-31 13:16:07 +0200

    refactor(backend0): item dto

--- ts/backend0/src/items/items.controller.ts
+++ ts/backend0/src/items/items.controller.ts
@@ -7,7 +7,7 @@ import {
   Body,
   Param,
 } from '@nestjs/common';
-import { CreateItemDto } from './dto/create-item.dto';
+import { ItemDto } from './dto/item.dto';
 import { ItemsService } from './items.service';
 import { Item } from './interfaces/item.interface';
 @Controller('items')
@@ -25,7 +25,7 @@ export class ItemsController {
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
+  create(@Body() createItemDto: ItemDto): string {
     return `Name: ${createItemDto.name}\nDescription: ${createItemDto.description}\nQuantity: ${createItemDto.qty}`;
   }
 
@@ -35,7 +35,7 @@ export class ItemsController {
   }
 
   @Put(':id')
-  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+  update(@Body() updateItemDto: ItemDto, @Param('id') id): string {
     return `Update ${id} - Name: ${updateItemDto.name}`;
   }
 }
